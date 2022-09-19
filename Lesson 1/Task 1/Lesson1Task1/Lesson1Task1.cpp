9491626f315e51658129eaf4cce041cb8109ed12
Helena Moore <moore.helena@yandex.ru>
2022-09-19 11:30:36 +0300

    code correction

--- Lesson 1/Task 1/Lesson1Task1/Lesson1Task1.cpp
+++ Lesson 1/Task 1/Lesson1Task1/Lesson1Task1.cpp
@@ -1,6 +1,11 @@
 ﻿#include <iostream>
 #include <fstream>
 
+int* createArr(int size)
+{
+    int* arr = new int[size];
+    return arr;
+}
 
 int main(int argc, char** argv)
 {
@@ -8,23 +13,25 @@ int main(int argc, char** argv)
     std::ifstream read_file ("in.txt");
     std::ofstream write_file("out.txt");
 
-    int size1 = 0, size2 = 0;
+    int size1= 0, size2 = 0;
 
     if (read_file.is_open())
     {
         read_file >> size1;
-        int* arr1 = new int[size1];
+        int* arr1 = createArr(size1);
         for (int i = 0; i < size1; ++i)
         {
             read_file >> arr1[i];
         }
 
         read_file >> size2;
-        int* arr2 = new int[size2];
+        int* arr2 = createArr(size2);
         for (int i = 0; i < size2; ++i)
         {
             read_file >> arr2[i];
         }
+
+        read_file.close();
 
         write_file << size2 << std::endl;
         write_file << arr2[size2 - 1] << " ";
@@ -40,6 +47,8 @@ int main(int argc, char** argv)
         }
         write_file << arr1[0];
 
+        write_file.close();
+
         delete[] arr1;
         delete[] arr2;
 
@@ -49,7 +58,4 @@ int main(int argc, char** argv)
     {
          std::cout << "Ошибка открытия файла";
     }
-
-    read_file.close();
-    write_file.close();
 }
