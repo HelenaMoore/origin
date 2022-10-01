1b9393805b8a3d62887489632b0d24413d95d8fe
Helena Moore <moore.helena@yandex.ru>
2022-10-01 08:47:20 +0300

    add lesson 3 homeworks

---
+++ Lesson 3/Lesson3Task1/Lesson3Task1.cpp
@@ -0,0 +1,132 @@
+﻿#include <iostream>
+
+class Calculator
+{
+public:
+    double num1;
+    double num2;
+
+    double add()
+    {
+        double sum = num1 + num2;
+        return sum;
+    }
+
+    double multiply()
+    {
+        double mult = num1 * num2;
+        return mult;
+    }
+
+    double substruct_1_2()
+    {
+        double diff_1_2 = num1 - num2;
+        return diff_1_2;
+    }
+
+    double substruct_2_1()
+    {
+        double diff_2_1 = num2 - num1;
+        return diff_2_1;
+    }
+
+    double divide_1_2()
+    {
+        double division_1_2 = num1 / num2;
+        return division_1_2;
+    }
+
+    double divide_2_1()
+    {
+        double division_2_1 = num2 / num1;
+        return division_2_1;
+    }
+
+    bool set_num1(double num1)
+    {
+        if (num1 != 0)
+        {
+            this->num1 = num1;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool set_num2(double num2)
+    {
+        if (num2 != 0)
+        {
+            this->num2 = num2;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
+
+void printCalculator(Calculator &calc, double num1, double num2)
+{
+    std::cout << "num1 + num2 = " << calc.add() << std::endl;
+    std::cout << "num1 - num2 = " << calc.substruct_1_2() << std::endl;
+    std::cout << "num2 - num1 = " << calc.substruct_2_1() << std::endl;
+    std::cout << "num1 * num2 = " << calc.multiply() << std::endl;
+    std::cout << "num1 / num2 = " << calc.divide_1_2() << std::endl;
+    std::cout << "num2 / num1 = " << calc.divide_2_1() << std::endl;
+}
+
+void askNumber1(Calculator &calc, double num1)
+{
+    std::cout << "Введите num1: ";
+    std::cin >> num1;
+
+    if (calc.set_num1(num1) == false)
+    {
+        while (!calc.set_num1(num1))
+        {
+            std::cout << "Неверный ввод!" << std::endl << "Введите num1: ";
+            std::cin >> num1;
+        }
+    }
+}
+
+void askNumber2(Calculator &calc, double num2)
+{
+    std::cout << "Введите num2: ";
+    std::cin >> num2;
+
+    if (calc.set_num2(num2) == false)
+    {
+        while (!calc.set_num2(num2))
+        {
+            std::cout << "Неверный ввод!" << std::endl << "Введите num2: ";
+            std::cin >> num2;
+        }
+    }
+}
+
+int main(int argc, char** argv)
+{
+    Calculator calc;
+    
+    double num1 = 0, num2 = 0;
+
+    setlocale(LC_ALL, "Russian");
+
+    do
+    {
+
+        askNumber1(calc, num1);
+
+        askNumber2(calc, num2);
+
+        printCalculator(calc, num1, num2);
+
+    } while (true);
+
+    return 0;
+}
---
+++ Lesson 3/Lesson3Task2/Lesson3Task2.cpp
@@ -0,0 +1,108 @@
+﻿#include <iostream>
+
+class Counter
+{
+private:
+    int num = 1;
+public:
+    void increase_counter()
+    {
+        ++num;
+    }
+
+    void decrease_counter()
+    {
+        --num;
+    }
+
+    void print_number()
+    {
+        std::cout << num << std::endl;
+    }
+
+    Counter(int num)
+    {
+        this->num = num;
+    }
+
+    Counter() 
+    {
+        num = 1;
+    }
+};
+
+void useCounter(Counter &counter)
+{
+    std::string command;
+    bool check_exit = false;
+    do
+    {
+        std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+        std::cin >> command;
+
+        if (command == "+")
+        {
+            counter.increase_counter();
+        }
+        else if (command == "-")
+        {
+            counter.decrease_counter();
+        }
+        else if (command == "=")
+        {
+            counter.print_number();
+        }
+        else if (command == "x")
+        {
+            std::cout << "До свидания!" << std::endl;
+            check_exit = true;
+        }
+        else
+        {
+            std::cout << "Неверно введена команда. ";
+        }
+    } while (check_exit == false);
+}
+
+std::string initialQuestion()
+{
+    std::string answer;
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите 'да' или 'нет': ";
+    std::cin >> answer;
+    return answer;
+}
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251");
+    
+    int num = 0;
+    std::string answer = initialQuestion();
+    bool check_answer = false;
+
+    do
+    {
+        if (answer == "да")
+        {
+            std::cout << "Введите начальное значение счётчика: ";
+            std::cin >> num;
+            Counter counter(num);
+            useCounter(counter);
+            check_answer = true;
+        }
+        else if (answer == "нет")
+        {
+            Counter counter;
+            useCounter(counter);
+            check_answer = true;
+        }
+        else
+        {
+            std::cout << "Неверно введена команда. Введите 'да' или 'нет': ";
+            std::cin >> answer;       
+        }
+    } while (check_answer == false);
+
+    return 0;
+}
