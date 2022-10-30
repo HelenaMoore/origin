2a778c55e4729e809c7322cc92cfdb2d3c973a5a
Helena Moore <moore.helena@yandex.ru>
2022-10-30 17:24:04 +0300

    code correction

--- Lesson 9/Task 1/Lesson9Task1/Lesson9Task1.cpp
+++ Lesson 9/Task 1/Lesson9Task1/Lesson9Task1.cpp
@@ -12,14 +12,78 @@ public:
         denominator_ = denomenator;
     }
 
-    double division()
+    void simplification()
     {
-        return numenator_ / denominator_;
+        int sign = 1;
+
+        if (numenator_ < 0 && denominator_ < 0)
+        {
+            numenator_ = numenator_ * (-1);
+            denominator_ = denominator_ * (-1);
+        }
+        else if (denominator_ < 0)
+        {
+            sign = sign * (-1);
+            denominator_ = denominator_ * (-1);
+        }
+        else if (numenator_ < 0)
+        {
+            numenator_ = numenator_ * (-1);
+            sign = sign * (-1);
+        }
+
+        if (numenator_ != 0 && numenator_ != 1 && denominator_ != 1)
+        {
+            int x = 0;
+            if (numenator_ < denominator_)
+            {
+                x = numenator_;
+            }
+            else
+            {
+                x = denominator_;
+            }
+
+            for (int i = x; i > 1; --i)
+            {
+                if (numenator_ % i == 0 && denominator_ % i == 0)
+                {
+                    numenator_ = numenator_ / i;
+                    denominator_ = denominator_ / i;
+                }
+            }
+        }
+
+        numenator_ = sign * numenator_;
     }
 
-    bool operator==(Fraction other) { return division() == other.division(); }
+    bool comparison1(Fraction f1, Fraction f2)
+    {
+        f1.simplification();
+        f2.simplification();
+        bool check = false;
+        if (f1.numenator_ == f2.numenator_ && f1.denominator_ == f2.denominator_)
+        {
+            check = true;
+        }
+        return check;
+    }
+
+    bool comparison2(Fraction f1, Fraction f2)
+    {
+        f1.simplification();
+        f2.simplification();
+        bool check = false;
+        if (f1.numenator_ * f2.denominator_ > f2.numenator_ * f1.denominator_)
+        {
+            check = true;
+        }
+        return check;
+    }
+
+    bool operator==(Fraction other) { return comparison1(*this, other); }
     bool operator!=(Fraction other) { return !(*this == other); }
-    bool operator>(Fraction other) { return division() > other.division(); }
+    bool operator>(Fraction other) { return comparison2(*this, other); }
     bool operator<(Fraction other) { return other > *this; }
     bool operator>=(Fraction other) { return !(*this < other); }
     bool operator<=(Fraction other) { return !(*this > other); }
